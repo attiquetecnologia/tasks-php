144641a7f7b4acacd1a191eecd5fa831c6ddf4d2
attiquetecnologia <rodrigoatique>
2025-04-01 07:58:48 -0300

    sistema de tarefas

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Tarefas</title>
+</head>
+<body>
+
+<?php require 'menu.php'; ?>
+
+<main>
+    
+    <h1> Sistema de Tarefas 0.1</h1>
+
+</main>
+</body>
+</html>
